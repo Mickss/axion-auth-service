d4caf06d037c3151bcb0f111b4b8f449ebc74e01
Michał Guz <michal.guz.85@gmail.com>
2023-06-21 11:59:59 +0200

    Corrected pom.xml

--- src/main/java/org/micks/champmaker/auth/Controller.java
+++ src/main/java/org/micks/champmaker/auth/Controller.java
@@ -1,13 +1,11 @@
 package org.micks.champmaker.auth;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
 public class Controller {
 
     @GetMapping
