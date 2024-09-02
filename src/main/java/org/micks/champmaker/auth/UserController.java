79304f5d29b95b7c8cbab1eb6245970db3badfb9
Michał Guz <michal.guz.85@gmail.com>
2024-09-02 08:43:20 +0200

    DG-24 Add role Admin and Player

---
+++ src/main/java/org/micks/champmaker/auth/UserController.java
@@ -0,0 +1,23 @@
+package org.micks.champmaker.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired UserService userService;
+
+    @PutMapping(value = "/{userId}/promote", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void promoteToAdmin(@PathVariable String userId) {
+        //TODO check if logged in user is admin
+        userService.promoteToAdmin(userId);
+    }
+}
