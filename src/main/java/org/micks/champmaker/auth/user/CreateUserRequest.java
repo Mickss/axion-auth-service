fd1474da0044a9a4e9be9102fa6a637ed5e1b3f5
Michał Guz <michal.guz.85@gmail.com>
2025-06-17 18:20:04 +0200

    DG-83 Change username to email address
    (cherry picked from commit 07a5388b8bb8f61ea410c71857ed1b4b7a39f3b0)

--- src/main/java/org/micks/champmaker/auth/user/CreateUserRequest.java
+++ src/main/java/org/micks/champmaker/auth/user/CreateUserRequest.java
@@ -2,14 +2,14 @@ package org.micks.champmaker.auth.user;
 
 public class CreateUserRequest {
 
-    private String username;
+    private String email;
     private String password;
 
     public CreateUserRequest() {
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
     public String getPassword() {
